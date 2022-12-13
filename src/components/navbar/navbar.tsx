c2a0f523df71a3b4aac0b6630dbd34aca0cfb99a
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2022-12-13 20:13:48 +0700

    fix: change brand name

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -100,7 +100,7 @@ const Navbar: React.FC<Props> = (props) => {
       )}
 
       {isMenuOpen && (
-        <div className='w-full h-screen filter backdrop-blur-lg z-40'/>
+        <div className='w-full h-screen filter backdrop-blur-lg z-40' />
       )}
 
 
@@ -108,7 +108,7 @@ const Navbar: React.FC<Props> = (props) => {
       <Container>
         <div className='mobile:hidden laptop:flex justify-between items-center py-6'>
           <a className='text-white text-xl font-medium' href='/'>
-            Kim Sell.
+            PT. Kimsell.
           </a>
           <ul className='flex justify-center items-center gap-10'>
             {menu.map((item, index) => (
